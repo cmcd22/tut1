ec7d5e10f79a3f80c1ec338750495f49faa29267
cmcd22 <c.i.mcdonald.92@gmail.com>
2021-07-22 11:15:16 +1200

    Fixed bug

--- Greeter.java
+++ Greeter.java
@@ -17,10 +17,15 @@ public class Greeter {
   }
 
   public static void main(String[] args) {
-    String language = args[0];
-    String country = args[1];
-    String name = args[2];
-    Greeter greeter = new Greeter(language, country, name);
-    System.out.println(greeter.sayHello());
+	  if args.length != 3 {
+		  System.out.println("Incorrect number of arguments");
+	  } else {
+		  String language = args[0];
+		    String country = args[1];
+		    String name = args[2];
+		    Greeter greeter = new Greeter(language, country, name);
+		    System.out.println(greeter.sayHello());
+	  }
+    
   }
 }
